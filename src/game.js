d64b32670546fad0bc7bb24375c9380d7d7e1f25
Shikhar Mishra <shikhatmishra730@gmail.com>
2022-09-24 17:52:37 +0530

    create draw function

--- src/game.js
+++ src/game.js
@@ -1,8 +1,8 @@
 class Game {
   defaultOptions = {
-    cellSize: 10,
-    cellsX: 70,
-    cellsY: 70,
+    cellSize: 15,
+    cellsX: 40,
+    cellsY: 40,
     cellColor: '#FF9A24',
     gridColor: '#FFDBB0',
     canvasColor: '#38342E',
@@ -13,12 +13,54 @@ class Game {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.options = this.setOptions(options);
-    this.canvas.width = this.options.cellsX * this.options.cellSize;
-    this.canvas.height = this.options.cellsY * this.options.cellSize;
+    this.setCanvas();
     this.initGrid();
+    this.randomise();
     this.round = 0;
   }
-
+  setCanvas() {
+    this.canvas.width = this.options.cellsX * this.options.cellSize;
+    this.canvas.height = this.options.cellsY * this.options.cellSize;
+  }
+  draw() {
+    this.cleanGrid();
+    this.drawGrid();
+    this.drawCells();
+  }
+  drawCells() {
+    const gap = 0.5;
+    this.ctx.fillStyle = this.options.cellColor;
+    for (let i = 0; i < this.options.cellsX; i++) {
+      for (let j = 0; j < this.options.cellsY; j++) {
+        if (this.grid[i][j]) {
+          this.ctx.fillRect(
+            i * this.options.cellSize + 0.5,
+            j * this.options.cellSize + 0.5,
+            this.options.cellSize,
+            this.options.cellSize
+          );
+        }
+      }
+    }
+  }
+  cleanGrid() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+  drawGrid() {
+    const gap = 0.5;
+    for (let i = 0; i <= this.options.cellsX; i++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(i * this.options.cellSize + gap, 0);
+      this.ctx.lineTo(i * this.options.cellSize + gap, this.canvas.height);
+      this.ctx.stroke();
+    }
+    for (let j = 0; j <= this.options.cellsY; j++) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(0, j * this.options.cellSize + gap);
+      this.ctx.lineTo(this.canvas.width, j * this.options.cellSize + gap);
+      this.ctx.stroke();
+    }
+  }
   setOptions(options) {
     return { ...this.defaultOptions, ...options };
   }
@@ -28,6 +70,7 @@ class Game {
     for (let i = 0; i < this.options.cellsX; i++) {
       this.grid[i] = new Array(this.options.cellsY).fill(false);
     }
+    this.draw();
   }
 
   setPoints(points) {
@@ -35,6 +78,7 @@ class Game {
       const [x, y] = point;
       this.grid[x][y] = true;
     });
+    this.draw();
   }
 
   calculateNeighbours(x, y) {
@@ -79,10 +123,12 @@ class Game {
     }
     this.grid = buffer;
     this.round++;
+    this.draw();
   }
   clearGrid() {
     this.initGrid();
     this.round = 0;
+    this.draw();
   }
   randomise() {
     this.clearGrid();
@@ -91,6 +137,7 @@ class Game {
         this.grid[i][j] = Math.random() < 0.3;
       }
     }
+    this.draw();
   }
 }
 
