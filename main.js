33fab8fe03825189baea783e7a510339695ed539
Shikhar Mishra <shikhatmishra730@gmail.com>
2022-09-24 17:59:49 +0530

    add controls ids

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import './style.css';
 import Game from './src/game';
 
+const roundEl = document.getElementById('round-value');
+const playEl = document.getElementById('play');
+const nextEl = document.getElementById('next');
+const clearEL = document.getElementById('clear');
+const randomiseEL = document.getElementById('randomise');
+
 const game = new Game(document.getElementById('canvas'));
